3a702ba12d1a7de39f007b1e05daaf657fc560c2
Ronen <rokatan@cisco.com>
2017-05-18 22:40:11 +0300

    public folder

--- app/routes.js
+++ app/routes.js
@@ -49,9 +49,4 @@ module.exports = function (app) {
             getTodos(res);
         });
     });
-
-    // application -------------------------------------------------------------
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
 };
